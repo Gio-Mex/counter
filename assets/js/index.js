52eda9827d082b24ab4281595e588559f9628fe5
Giorgio Messore <giorgio.messore@live.it>
2023-07-18 18:26:49 +0200

    implementazione JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,58 @@
+// Initialization
+const counter = document.querySelector('[counter]')
+counter.insertAdjacentHTML('afterbegin',
+`
+<div display class="bg-info rounded w-100 mb-3 d-flex justify-content-center 
+  align-items-center"></div>
+<div class="d-flex flex-column">
+    <div class="d-flex flex-row mb-4">
+        <button addButton type="button" class="btn btn-success me-2 fs-1 w-50">+</button>
+        <button minusButton type="button" class="btn btn-danger ms-2 fs-1 w-50">-</button>
+    </div>
+    <button resetButton type="button" class="btn btn-primary fs-1 w-100">RESET</button>
+</div>
+`
+)
+
+const display = document.querySelector('[display]')
+let number = 0
+updateDisplay()
+
 const addBtn = document.querySelector('[addButton]')
 const minusBtn = document.querySelector('[minusButton]')
 const resetBtn = document.querySelector('[resetButton')
-const display = document.querySelector('[display]')
 
-// Initialization
-let number = 0
-display.innerText = number
 
 // Events
-addBtn.addEventListener('click', () => {
-    increase();
-    updateDisplay();
-})
-
-minusBtn.addEventListener('click', () => {
-    decrease();
-    updateDisplay();
-})
-
-resetBtn.addEventListener('click', () => {
-    reset();
-    updateDisplay();
+counter.addEventListener('click', function (event) {
+  switch (event.target) {
+    case addBtn:
+      increase();
+      break;
+    case minusBtn:
+      decrease();
+      break;
+    case resetBtn:
+      reset();
+      break;
+  }
+  updateDisplay()
 }
-)
+);
 
 // Functions
 function increase() {
-    number += 1;
+  number++;
 }
 
 function decrease() {
-    number -= 1;
+  number--;
 }
 
 function reset() {
-    number = confirm("Do you really want to reset the count?") ? 0 : exit;
+  number = confirm("Do you really want to reset the count?") ? 0 : number;
 }
 
 function updateDisplay() {
-    display.innerText = number;
+  display.textContent = number;
 }
